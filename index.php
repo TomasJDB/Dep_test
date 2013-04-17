fc44eb2443fbc149151af3cbbc64b8e46fb9044a
Tomas <tomas@jdb.lt>
2013-04-17 13:13:08 +0300

    The beginning. Testing dep

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Aketuriarchitektai.lt | Home</title>
+        <link href="/css/font.css" rel="stylesheet" type="text/css">
+        <link href="/css/main.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+        <!--wrapper starts-->
+        <div id="wrapper">
+
+            <!--header starts-->
+            <div class="header">
+                <ul class="nav-bar-left">
+                    <li class="nav-home" >
+                        <a href="/"><span id="fix-minus-spacing">-</span>Aketuri architektai</a>
+                    </li>
+                    <li class="nav-team">
+                        <a href="/"><div></div></a>
+                    </li>
+                    <li class="nav-blog">
+                        <a href="/"><div></div></a>
+                    </li>
+                </ul>
+                <ul class="nav-bar-right">
+                    <li class="nav-fix-ul-spacing">
+                        <div></div>
+                    </li>
+                    <li class="nav-address">
+                        <a href="/">Šv. Stepono g. 18, Vilnius</a>
+                    </li>
+                    <li class="nav-email">
+                        <a href="/">INFO@AKETURIARCHITEKTAI.LT</a>
+                    </li>
+                </ul>
+                <div class="clear"></div>
+            </div>
+            <!--header ends-->
+
+            <!--container starts-->
+            <div id="container">
+                <!--left column starts-->
+                <div class="column-left-min">
+
+                </div>
+                <!--left column ends-->
+                
+                <!--middle small column starts-->
+                <div class="column-mid-min small">
+                    
+                </div>
+                <!--middle small column ends-->
+                
+                <!--right small column starts-->
+                <div class="column-right-min small">
+
+                </div>
+                <!--right small column ends-->
+                
+                <!--column big middle and right starts-->
+                <div>
+
+                </div>
+                <!--column big middle and right ends-->
+            </div>
+            <!--container ends-->
+        </div>
+        <!--wrapper ends-->
+    </body>
+</html>
